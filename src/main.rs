ceaf8d31ac0bb4ae4927bddeeb2c1c526e22bbe3
Evgeniy Nekrasov Shetinin <enshetinin@gmail.com>
2026-07-22 20:39:51 +0200

    chore: rest toolchain and testing stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+fn build_welcome_message(project_name: &str) -> String {
+    format!("Project {project_name} prepared to start")
+}
 fn main() {
-    println!("Hello, world!");
+    let project_name = "Personal Issue Tracker";
+    let message = build_welcome_message(project_name);
+
+    println!("{message}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::build_welcome_message;
+
+    #[test]
+    fn build_welcome_message_witch_project_name() {
+        let message = build_welcome_message("Cool");
+        assert_eq!(message, "Project Cool prepared to start");
+    }
 }
